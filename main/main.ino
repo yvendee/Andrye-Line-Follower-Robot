6ec2ae807bac19716d33b32a4f3ab1d6b90cd7bd
yvendee <71810479+yvendee@users.noreply.github.com>
2024-04-22 14:50:06 +0800

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -123,21 +123,21 @@ void loop(){
     stopMotors();
   }
 
-  // Clears the trigPin
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  // duration1 = pulseIn(echoPin, HIGH, 1000000);
-  duration1 = pulseIn(echoPin, HIGH, 10000);
-  // Calculating the distance
-  distance1 = duration1 * 0.034 / 2;
-  // Prints the distance on the Serial Monitor
-  Serial.print("Distance: ");
-  Serial.println(distance1);
+  // // Clears the trigPin
+  // digitalWrite(trigPin, LOW);
+  // delayMicroseconds(2);
+  // // Sets the trigPin on HIGH state for 10 micro seconds
+  // digitalWrite(trigPin, HIGH);
+  // delayMicroseconds(10);
+  // digitalWrite(trigPin, LOW);
+  // // Reads the echoPin, returns the sound wave travel time in microseconds
+  // // duration1 = pulseIn(echoPin, HIGH, 1000000);
+  // duration1 = pulseIn(echoPin, HIGH, 10000);
+  // // Calculating the distance
+  // distance1 = duration1 * 0.034 / 2;
+  // // Prints the distance on the Serial Monitor
+  // Serial.print("Distance: ");
+  // Serial.println(distance1);
 
   char key = keypad.getKey();// Read the key
   
